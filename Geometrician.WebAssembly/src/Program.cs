ee07ab01ddd51765efbb7aa92448fbb7716f1a0b
Laurence Elsdon <git@elsdon.me>
2023-01-14 20:06:16 +0000

    fix: Update Geometrician.WebAssembly following previous

--- Geometrician.WebAssembly/src/Program.cs
+++ Geometrician.WebAssembly/src/Program.cs
@@ -2,9 +2,20 @@ using Geometrician.Components.Composition;
 using Geometrician.Components.Scripts;
 using Geometrician.Components.Shared;
 using Geometrician.Components.Visualization;
+using Geometrician.Core.Assets;
+using Geometrician.Core.Configuration;
 using Geometrician.Core.Execution;
+using Geometrician.Core.Samples;
+using Geometrician.Core.Visualization;
 using Geometrician.WebAssembly;
+using Lineweights.Core.Assets;
 using Lineweights.Core.Storage;
+using Lineweights.Drawings;
+using Lineweights.Drawings.Samples;
+using Lineweights.Flex.Samples;
+using Lineweights.IFC;
+using Lineweights.PDF;
+using Lineweights.SVG;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -19,16 +30,50 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // Add MudBlazor
 builder.Services.AddMudServices();
 
-// Inject Lineweights singleton services
+// Inject general Geometrician services
 builder.Services.AddSingleton<VisualizationState>();
-builder.Services.AddSingleton<ObjectUrlStorage>();
-builder.Services.AddSingleton<IStorageStrategy, ObjectUrlStorageStrategy>();
-builder.Services.AddSingleton<ModelViewer>();
-builder.Services.AddSingleton<IActivityFactory, StaticMethodActivityFactory>();
-builder.Services.AddTransient<DisplayState>();
-
-// Inject Lineweights scoped services
+builder.Services.AddTransient<ModelViewer>();
 builder.Services.AddScoped<CompositionState>();
+builder.Services.AddScoped<CommunicationState>();
+builder.Services.AddTransient<ObjectUrlStorage>();
+builder.Services.AddTransient<DisplayState>();
+builder.Services.AddTransient<AssemblyResolver>(_ => new AssemblyResolverBuilder()
+    .Register(typeof(SheetSample).Assembly)
+    .Register(typeof(AssetTypes).Assembly)
+    .Register(typeof(WallFlemishBond).Assembly)
+    .Build());
+builder.Services.AddTransient<AssetFactoryResolver>(services => new AssetFactoryResolverBuilder(services)
+    .Register<Model, GlbAssetFactory>()
+    .Register<ExternalAsset, AssetFactory>()
+    .Register<InternalAsset, AssetFactory>()
+    .Register<Sheet, SvgAssetFactory<Sheet>>()
+    .Register<View, SvgAssetFactory<View>>()
+    .Register<Model, CsvElementTypesAssetFactory>()
+    .Register<Sheet, PdfAssetFactory<Sheet>>()
+    .Register<View, PdfAssetFactory<View>>()
+    .Register<Model, IfcAssetFactory>()
+    .Register<Model, JsonAssetFactory>()
+    .Build());
+builder.Services.AddTransient<ViewerComponentResolver>(_ => new ViewerComponentResolverBuilder()
+    .Register("application/pdf", typeof(ObjectViewerComponent))
+    .Register("model/gltf-binary", typeof(ThreeViewerComponent))
+    .Register("text/csv", typeof(TableViewerComponent))
+    .Register("text/plain", typeof(TextViewerComponent))
+    .Build());
+builder.Services.AddTransient<VisualizationConfiguration>();
+builder.Services.AddTransient<GlbAssetFactory>();
+builder.Services.AddTransient<AssetFactory>();
+builder.Services.AddTransient<SvgAssetFactory<Sheet>>();
+builder.Services.AddTransient<SvgAssetFactory<View>>();
+builder.Services.AddTransient<CsvElementTypesAssetFactory>();
+builder.Services.AddTransient<PdfAssetFactory<Sheet>>();
+builder.Services.AddTransient<PdfAssetFactory<View>>();
+builder.Services.AddTransient<IfcAssetFactory>();
+builder.Services.AddTransient<JsonAssetFactory>();
+
+// Inject environment specific Geometrician services
+builder.Services.AddTransient<IActivityFactory, StaticMethodActivityFactory>();
+builder.Services.AddTransient<IStorageStrategy, ObjectUrlStorageStrategy>();
 
 // Inject Blazor WebAssembly services
 builder.Services.AddScoped(_ => new HttpClient
