78b666291128e9c06d760baff7b503daf9ff5db7
Laurence Elsdon <git@elsdon.me>
2023-04-29 20:35:28 +0100

    feat(Geometrician.Cascade.Extensions): Added unified IServiceCollection extension methods

--- Geometrician.Cascade.Server/src/Program.cs
+++ Geometrician.Cascade.Server/src/Program.cs
@@ -1,19 +1,6 @@
-using Geometrician.Cascade.Components.Composition;
-using Geometrician.Cascade.Components.Scripts;
-using Geometrician.Cascade.Components.Shared;
-using Geometrician.Cascade.Components.Visualization;
-using Geometrician.Core.Assets;
+using Geometrician.Cascade.Extensions;
 using Geometrician.Core.Storage;
-using Geometrician.Drawings;
-using Geometrician.IFC;
-using Geometrician.PDF;
-using Geometrician.SVG;
-using Geometrician.Workflows.Assets;
-using Geometrician.Workflows.Configuration;
 using Geometrician.Workflows.Storage;
-using Geometrician.Workflows.Visualization;
-using MudBlazor;
-using MudBlazor.Services;
 using StudioLE.Workflows;
 using StudioLE.Workflows.Abstractions;
 using StudioLE.Workflows.Providers;
@@ -25,57 +12,21 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-// Add MudBlazor
-builder.Services.AddMudServices(config =>
-{
-    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
-    config.SnackbarConfiguration.RequireInteraction = true;
-});
-
-// Inject Geometrician configuration
+// Inject configuration
+// TODO: Refactor configuration
 builder.Services.Configure<ActivitiesOptions>(
     builder.Configuration.GetSection(ActivitiesOptions.Section));
 
-// Inject Geometrician services
-builder.Services.AddSingleton<VisualizationState>();
-builder.Services.AddTransient<ModelViewer>();
-builder.Services.AddScoped<CompositionState>();
-builder.Services.AddScoped<CommunicationState>();
-builder.Services.AddTransient<ObjectUrlStorage>();
-builder.Services.AddTransient<DisplayState>();
-builder.Services.AddTransient<AssemblyResolver>();
-builder.Services.AddTransient<AssetFactoryResolver>(services => new AssetFactoryResolverBuilder(services)
-    .Register<Model, GlbAssetFactory>()
-    .Register<ExternalAsset, AssetFactory>()
-    .Register<InternalAsset, AssetFactory>()
-    .Register<Sheet, SvgAssetFactory<Sheet>>()
-    .Register<View, SvgAssetFactory<View>>()
-    .Register<Model, CsvElementTypesAssetFactory>()
-    .Register<Sheet, PdfAssetFactory<Sheet>>()
-    .Register<View, PdfAssetFactory<View>>()
-    .Register<Model, IfcAssetFactory>()
-    .Register<Model, JsonAssetFactory>()
-    .Build());
-builder.Services.AddTransient<ViewerComponentResolver>(_ => new ViewerComponentResolverBuilder()
-    .Register("application/pdf", typeof(ObjectViewerComponent))
-    .Register("model/gltf-binary", typeof(ThreeViewerComponent))
-    .Register("text/csv", typeof(TableViewerComponent))
-    .Register("text/plain", typeof(TextViewerComponent))
-    .Build());
-builder.Services.AddTransient<VisualizationConfiguration>();
-builder.Services.AddTransient<GlbAssetFactory>();
-builder.Services.AddTransient<AssetFactory>();
-builder.Services.AddTransient<SvgAssetFactory<Sheet>>();
-builder.Services.AddTransient<SvgAssetFactory<View>>();
-builder.Services.AddTransient<CsvElementTypesAssetFactory>();
-builder.Services.AddTransient<PdfAssetFactory<Sheet>>();
-builder.Services.AddTransient<PdfAssetFactory<View>>();
-builder.Services.AddTransient<IfcAssetFactory>();
-builder.Services.AddTransient<JsonAssetFactory>();
-
-// Inject Lineweights services
+// Inject standard Geometrician services
+builder.Services.AddComponentServices();
+builder.Services.AddDefaultAssetFactoryServices();
+builder.Services.AddDefaultViewerComponentServices();
+builder.Services.AddConfigurationServices();
 builder.Services.AddTransient<IActivityResolver, ActivityResolver>();
+
+// Inject Blazor Server specific services
 builder.Services.AddTransient<IStorageStrategy, BlobStorageStrategy>();
+builder.Services.AddTransient<AssemblyResolver>();
 
 // Build application
 WebApplication app = builder.Build();
