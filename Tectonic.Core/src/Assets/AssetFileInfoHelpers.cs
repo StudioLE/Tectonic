031504054fdddd051c5be23f4df6786460d8300e
Laurence Elsdon <git@elsdon.me>
2024-04-16 22:35:46 +0100

    feat(Tectonic.Core): Added AssetFileInfoHelpers

---
+++ Tectonic.Core/src/Assets/AssetFileInfoHelpers.cs
@@ -0,0 +1,71 @@
+using System.Text;
+
+namespace Tectonic.Assets;
+
+/// <summary>
+/// Methods to help with <see cref="IAssetFileInfo"/>.
+/// </summary>
+public static class AssetFileInfoHelpers
+{
+    /// <summary>
+    /// Determine if the asset includes inline data encoded is a data URI.
+    /// </summary>
+    /// <param name="asset">The asset.</param>
+    /// <returns>
+    /// <see langword="true"/> if the asset location is a data uri; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool IsInlineData(this IAssetFileInfo asset)
+    {
+        return asset.Location.StartsWith("data:");
+    }
+
+    /// <summary>
+    /// Determine if the asset is a local file.
+    /// </summary>
+    /// <param name="asset">The asset.</param>
+    /// <returns>
+    /// <see langword="true"/> if the asset location is a data uri; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool IsLocalFile(this IAssetFileInfo asset)
+    {
+        return asset.Location.StartsWith("file:");
+    }
+
+    /// <summary>
+    /// Get the inline data from the asset's data URI.
+    /// </summary>
+    /// <param name="asset">The asset.</param>
+    /// <returns>
+    /// <see langword="true"/> if the asset location is a data uri; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static string GetInlineData(this IAssetFileInfo asset)
+    {
+        if (!IsInlineData(asset))
+            return string.Empty;
+        string[] parts = asset.Location.Split(',');
+        string metadata = parts[0];
+        string data = string.Join("", parts.Skip(1));
+        if (!metadata.Contains(";base64"))
+            return data;
+        return Encoding.UTF8.GetString(Convert.FromBase64String(data));
+    }
+
+    /// <summary>
+    /// Get the inline data from the asset's data URI.
+    /// </summary>
+    /// <param name="asset">The asset.</param>
+    /// <returns>
+    /// <see langword="true"/> if the asset location is a data uri; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static byte[] GetInlineDataBytes(this IAssetFileInfo asset)
+    {
+        if (!IsInlineData(asset))
+            return Array.Empty<byte>();
+        string[] parts = asset.Location.Split(',');
+        string metadata = parts[0];
+        string data = string.Join("", parts.Skip(1));
+        if (!metadata.Contains(";base64"))
+            return Encoding.UTF8.GetBytes(data);;
+        return Convert.FromBase64String(data);
+    }
+}
--- Tectonic.Core/tests/Assets/AssetFileInfoTests.cs
+++ Tectonic.Core/tests/Assets/AssetFileInfoTests.cs
@@ -2,6 +2,7 @@ using Newtonsoft.Json;
 using NUnit.Framework;
 using StudioLE.Diagnostics;
 using StudioLE.Diagnostics.NUnit;
+using StudioLE.Extensions.System;
 using StudioLE.Verify;
 using Tectonic.Assets;
 
@@ -41,4 +42,103 @@ internal sealed class AssetFileInfoTests
             Assert.That(deserialized?.Location, Is.EqualTo(asset.Location), "Location");
         });
     }
+
+    [TestCase("https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top", false)]
+    [TestCase("mailto:John.Doe@example.com", false)]
+    [TestCase("tel:+1-816-555-1212", false)]
+    [TestCase("file://localhost/c:/WINDOWS/clock.avi", false)]
+    [TestCase("file:///c:/WINDOWS/clock.avi", false)]
+    [TestCase("urn:oasis:names:specification:docbook:dtd:xml:4.1.2", false)]
+    [TestCase("file://localhost/etc/fstab", false)]
+    [TestCase("file:///etc/fstab", false)]
+    [TestCase("file:/etc/fstab", false)]
+    [TestCase("data:image/jpeg;base64,/9j/4AAQSkZJRgABAgAAZABkAAD", true)]
+    public void AssetFileInfoHelpers_IsInlineData(string location, bool expected)
+    {
+        // Arrange
+        AssetFileInfo asset = new()
+        {
+            Location = location
+        };
+
+        // Act
+        bool result = asset.IsInlineData();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase("https://john.doe@www.example.com:123/forum/questions/?tag=networking&order=newest#top", false)]
+    [TestCase("mailto:John.Doe@example.com", false)]
+    [TestCase("tel:+1-816-555-1212", false)]
+    [TestCase("file://localhost/c:/WINDOWS/clock.avi", true)]
+    [TestCase("file:///c:/WINDOWS/clock.avi", true)]
+    [TestCase("urn:oasis:names:specification:docbook:dtd:xml:4.1.2", false)]
+    [TestCase("file://localhost/etc/fstab", true)]
+    [TestCase("file:///etc/fstab", true)]
+    [TestCase("file:/etc/fstab", true)]
+    public void AssetFileInfoHelpers_IsLocalFile(string location, bool expected)
+    {
+        // Arrange
+        AssetFileInfo asset = new()
+        {
+            Location = location
+        };
+
+        // Act
+        bool result = asset.IsLocalFile();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void AssetFileInfoHelpers_GetInlineData_UTF8()
+    {
+        // Arrange
+        AssetFileInfo asset = new()
+        {
+            Location = "data:text/plain;charset=UTF-8;page=21,the%20data:1234,5678"
+        };
+
+        // Act
+        string result = asset.GetInlineData();
+
+        // Assert
+        Assert.That(result, Is.EqualTo("the%20data:12345678"));
+    }
+
+    [Test]
+    public async Task AssetFileInfoHelpers_GetInlineData_Svg()
+    {
+        // Arrange
+        AssetFileInfo asset = new()
+        {
+            Location = "data:image/svg+xml;utf8,\n<svg width='10' height='10' xmlns='http://www.w3.org/2000/svg'>\n <circle style='fill:red' cx='5' cy='5' r='5'/>\n</svg>"
+        };
+
+        // Act
+        string result = asset.GetInlineData();
+
+        // Assert
+        await _context.Verify(result);
+    }
+
+    [Test]
+    public async Task AssetFileInfoHelpers_GetInlineDataBytes_Base64()
+    {
+        // Arrange
+        AssetFileInfo asset = new()
+        {
+            Location = "data:image/png;base64,iVBORw0KGgoAAA\nANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4\n//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU\n5ErkJggg=="
+        };
+
+        // Act
+        byte[] result = asset.GetInlineDataBytes();
+
+        // Assert
+        await _context.Verify(result.Select(x => x.ToString()).Join());
+    }
+
+
 }
